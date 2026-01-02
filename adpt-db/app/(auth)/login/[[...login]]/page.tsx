f31e1edc7483d66d7b20bf94ad7595ec9c628ce8
Vaghela_Jenil <jenilvaghela9008@gmail.com>
2026-01-03 00:17:55 +0530

    create clerk login, signup pages

---
+++ adpt-db/app/(auth)/login/[[...login]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="w-full max-w-md p-6 rounded-xl">
+                <SignIn routing='path' path='/login' forceRedirectUrl='/dashboard'/>
+            </div>
+        </div>
+
+    )
+}
---
+++ adpt-db/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function DashBoard() {
+  return (
+     <div className="min-h-screen flex items-center justify-center">
+                <div className="w-full max-w-md p-6 rounded-xl">
+                   <h1>DashBoard</h1>
+                </div>
+            </div>
+  )
+}
